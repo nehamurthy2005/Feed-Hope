dd52fd4cba38405ecff56a8c4f36a91614ecfc74
nehamurthy2005 <n.neha10092005@gmail.com>
2026-05-17 19:33:12 +0530

    Updated CORS for Vercel frontend

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://feed-hope-taupe.vercel.app"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
