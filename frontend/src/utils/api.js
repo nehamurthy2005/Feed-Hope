3bae6afd6b86120f573569f7c78910f7e604f082
nehamurthy2005 <n.neha10092005@gmail.com>
2026-05-17 19:10:26 +0530

    Update frontend API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "/api" });
+const API = axios.create({
+  baseURL: "https://feed-hope-backend.onrender.com/api",
+});
 
 // Always attach the latest token from localStorage
 API.interceptors.request.use((config) => {
